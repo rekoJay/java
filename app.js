ea4792618ad57428fc357787a1305f1caab068f4
rekoJay <132415718+rekoJay@users.noreply.github.com>
2023-06-07 16:13:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert("hi");
+const a = 5;
+const b = 2;
+let myName = "Jay"
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hi " + myName)
+
+myName = "Jays"
+console.log("Hello " + myName)
