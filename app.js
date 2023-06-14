07c69c274cbc33489b278d91f3272c382307561f
rekoJay <132415718+rekoJay@users.noreply.github.com>
2023-06-15 08:41:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === "blue"){
-        newColor = "tomato";
-    } else {
-        newColor = "blue";
-    }
-    h1.style.color = newColor;
+    const clickedClass = "active";
+if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
+} else {
+    h1.classList.add(clickedClass);
+}
 }
 h1.addEventListener("click",handleTitleClick);
