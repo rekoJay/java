75cdc1342b3bfd4ade3e4020a3097c296977c592
rekoJay <132415718+rekoJay@users.noreply.github.com>
2023-06-14 17:13:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,35 @@ function handleTitleClick() {
     title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+    title.innerText = "mouse is here!";
+}
+
+function handleMouseLeave() {
+    title.innerText = "mouse is gone!";
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!")
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI")
+}
+
+function handleWindowOnline() {
+    alert("ALL GOOOOD")
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
